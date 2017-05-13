b1c79a66c8f7b2fd9db119de7b0fb93e1ecacec5
AndyWand <andreaswandert@gmail.com>
2017-05-13 11:54:15 +0200

    added ConfigurationReader and Configuration Class (not used yet)

--- src/main/java/de/hsbo/copernicus/datasource/AbstractAdapter.java
+++ src/main/java/de/hsbo/copernicus/datasource/AbstractAdapter.java
@@ -8,6 +8,7 @@ package de.hsbo.copernicus.datasource;
 import math.geom2d.polygon.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Calendar;
 import java.util.HashMap;
 
@@ -21,17 +22,27 @@ public abstract class AbstractAdapter implements Runnable {
      * instance is an object of class DataSource to ensure that there is only one instance of each
      * adapter existing
      */
+    public String name;
     private static AbstractAdapter instance;
-    private static String baseURL; 
-    private static File result;    
-    
-    
-     /**
+    public String baseUrl;
+    private static File result;
+
+    /**
      * Method to test waether a Portal is available
      *
      * @return
      */
-    abstract public boolean isOnline();
+    public boolean isOnline() {
+        // check if service at 'baseURL' is available
+        String[] urlArray1 = baseUrl.split("//");
+        String[] urlArray2 = urlArray1[1].split("/");
+        try {
+            InetAddress.getByName(urlArray2[0]).isReachable(3000); //Replace with your name
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
 
     /**
      *
@@ -55,5 +66,7 @@ public abstract class AbstractAdapter implements Runnable {
      * @return file: file object passed in setQuery
      */
     public abstract File getResult();
+
+    public abstract String getName();
 
 }
